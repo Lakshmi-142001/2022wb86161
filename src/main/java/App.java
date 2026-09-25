59fa59e83d75ca10179599cf13c6152e2b4594c9
Lakshmi-142001 <2022wb86161@wilp.bits-pilani.ac.in>
2026-09-25 15:45:47 +0530

    Modified App.java for continuous integration

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,7 +1,7 @@
 public class App {
  
     public static void main(String[] args) {
-        System.out.println("Hello Jenkins!");
+        System.out.println("Hello Jenkins - Continuous Integration Test!");
         System.out.println("Continuous Integration using Git, Maven and Jenkins");
     }
  
