e649189825502913d5ca2778986c9470c24b84c7
Lakshmi-142001 <2022wb86161@wilp.bits-pilani.ac.in>
2026-09-25 15:24:53 +0530

    Added App.java

---
+++ src/main/java/App.java
@@ -0,0 +1,8 @@
+public class App {
+ 
+    public static void main(String[] args) {
+        System.out.println("Hello Jenkins!");
+        System.out.println("Continuous Integration using Git, Maven and Jenkins");
+    }
+ 
+}
